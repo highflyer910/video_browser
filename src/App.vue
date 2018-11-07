dd85c9293b69aacc1b254edf7ecfe600f43441a4
Thea <thea.mushambadze@gmail.com>
2018-11-07 22:51:56 +0400

    update

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,31 @@
 <template>
-	<div>Hi there!</div>
+	<div>
+		<SearchBar @termChange="onTermChange"></SearchBar>
+	</div>
 </template>
 
 <script>
+	import axios from 'axios';
+    import SearchBar from './components/SearchBar';
+    const API_KEY = 'AIzaSyAekfRS0Hps1k4mCYSUHzv0V7dVVxKVqdg';
+
 	export default{
-		name: 'App';
+		name: 'App',
+		components: {
+			SearchBar
+		},
+		methods: {
+			onTermChange(searchTerm){
+               axios.get('https://www.googleapis.com/youtube/v3/search', {
+               	params: {
+               		key: API_KEY,
+               		type: 'video',
+               		part: 'snippet',
+               		q: searchTerm
+               	}
+              }).then(response => console.log(response));
+			}
+		}
 
 	};
 </script>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<input @input="onInput"/>
+	</div>
+</template>
+
+
+<script>
+	export default{
+		name: 'SearchBar',
+		methods: {
+			onInput: function(event){
+				this.$emit('termChange', event.target.value);
+
+			}
+		}
+	};
+</script>
+
+
+<style>
+	
+</style>
